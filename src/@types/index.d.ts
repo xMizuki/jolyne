6ffc4dc4d9d203ddc1924453cf8b137428027a72
Mizuki <62658861+xMizuki@users.noreply.github.com>
2022-04-15 17:14:54 +0200

    ✏️ Removed some useless interfaces, updated some things

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import Client from '../base/Client';
+import Client from '../structures/Client';
 import type { CommandInteraction, Interaction, Message } from 'discord.js';
 
 /**
